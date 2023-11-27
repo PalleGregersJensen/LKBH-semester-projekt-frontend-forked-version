86bb69a427193f8b9ac3fb66406de31174cc0ff2
Jon <lundbyjon@hotmail.com>
2023-11-27 14:33:54 +0100

    SPA virker med links,. Næste skridt er at koble login sammen med viewChange()

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 "use strict";
 
+// ===== IMPORTS ===== \\
+import { initViews } from "./view-router.js";
+
 window.addEventListener("load", initApp);
 
 function initApp() {
     console.log("JavaScript is live! 🎉");
+    initViews();
 }
