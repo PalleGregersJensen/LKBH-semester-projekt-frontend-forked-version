749bbf8f416f05db4cf54348305e8714d85f7f5b
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-11-27 12:19:46 +0100

    Login funktionalitet virker vist fornuftigt. Ekstra data sendes med fra backend til frontend og modtages i frontend.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,10 @@
 import { endpoint, initApp } from "./main.js";
 
+let isLoggedIn;
+console.log(isLoggedIn);
+let isAdmin;
+console.log(isAdmin);
+
 // login clicked
 async function loginClicked() {
     console.log("login clicked");
@@ -8,10 +13,18 @@ async function loginClicked() {
     console.log(userName);
     const password = form.password.value;
     console.log(password);
-    const checkedUsernnameAndPassword = await checkUsernameAndPassword(userName, password);
-    console.log(checkedUsernnameAndPassword);
+    const checkedUsernameAndPassword = await checkUsernameAndPassword(userName, password);
+    console.log(checkedUsernameAndPassword.isAdmin);
+    // Checks if user logged in is admin or substitute
+    if (checkedUsernameAndPassword.isAdmin === 1) {
+        isAdmin = true;
+    } else {
+        isAdmin = false;
+    }
+    console.log(isAdmin);
 }
 
+// Post response to backend to see, if entered username and password is correct
 async function checkUsernameAndPassword(enteredUserName, enteredPassword) {
     console.log("check username and password");
     console.log(enteredUserName);
@@ -28,8 +41,13 @@ async function checkUsernameAndPassword(enteredUserName, enteredPassword) {
     });
 
     if (response.ok) {
-        // if success, run start
-        return response;
+        // if response ok, set isLoggedIn = true and return JSON data
+        isLoggedIn = true;
+        console.log(isLoggedIn);
+        // retrieve JSON-data from backend
+        const data = await response.json();
+        console.log(data);
+        return data;
     } else {
         console.log("Login fejlede");
     }
