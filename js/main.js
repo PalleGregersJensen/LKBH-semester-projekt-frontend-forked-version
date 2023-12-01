75fcf4bf609c31397ab5aa7ed48f603c15bd0299
Magnus Carstensen <brondby_1964@hotmail.com>
2023-12-01 11:53:07 +0100

    reference øndret

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ async function initApp() {
 
             const specificSubstitute = substitutes.filter((substitute) => substitute.EmployeeID === loggedInEmployeeID);
             // console.log(specificSubstitute);
-            const substitute = new ListRenderer(specificSubstitute, "#mypage", substituteRenderer);
+            const substitute = new ListRenderer(specificSubstitute, ".myinfo", substituteRenderer);
             substitute.render();
             substituteRenderer.attachEventListener(substitute);
 
