89794b03667d93bbf8a2ccbbd586605880b1dd10
Magnus Carstensen <brondby_1964@hotmail.com>
2023-12-01 10:13:05 +0100

    helpers.js lavet

---
+++ js/helpers.js
@@ -0,0 +1,32 @@
+import {endpoint} from "./main.js"
+async function createShiftInterest(ShiftID, EmployeeID){
+    const newShiftInterest = {ShiftID, EmployeeID};
+
+    const newShiftInterestJSON = JSON.stringify(newShiftInterest);
+    console.log(newShiftInterestJSON);
+    const response = await fetch(`${endpoint}/shiftInterests`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: newShiftInterestJSON,
+    });
+    return response;
+}
+
+async function updateLoginInfo(username, password, EmployeeID){
+    const newLoginInfo = {Username: username, PasswordHash: password};
+
+    const newLoginInfoJSON = JSON.stringify(newLoginInfo);
+    console.log(newLoginInfoJSON);
+    const response = await fetch(`${endpoint}/substitutes/${EmployeeID}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: newLoginInfoJSON,
+    });
+    return response;
+}
+
+export {createShiftInterest, updateLoginInfo};
