560834000706c36df03b06b9714aa1200dfb869c
MarkusIngerslev <markusolsen2004@gmail.com>
2023-12-12 21:35:56 +0100

    Tilføjet mere på endelig report, således at den nu giver bedre mening. (mangler stadig billed af MVC)

--- js/listrenderer.js
+++ js/listrenderer.js
@@ -1,29 +1,30 @@
 export class ListRenderer {
     constructor(list, container, itemRenderer, buttonID1, buttonID2) {
-      this.items = list;
-      this.container = document.querySelector(container);
-      this.itemRenderer = itemRenderer;
-      this.buttonID1 = buttonID1;
-      this.buttonID2 = buttonID2;
-      this.filteredItems = []; // Initialize filteredItems array
+        this.items = list;
+        this.container = document.querySelector(container);
+        this.itemRenderer = itemRenderer;
+        this.buttonID1 = buttonID1;
+        this.buttonID2 = buttonID2;
+        this.filteredItems = []; // Initialize filteredItems array
     }
 
     render() {
-      this.container.innerHTML = "";
-      const filteredList = this.items.filter(item => this.filterValue === "all" || item[this.filterProperty] == this.filterValue);
-      for (const item of filteredList) {
-          const html = this.itemRenderer.render(item);
-          this.container.insertAdjacentHTML("beforeend", html);
-  
-          if (this.itemRenderer.postRenderer) {
-              const element = this.container.lastElementChild;
-              const button1 = element.querySelector(this.buttonID1);
-              const button2 = element.querySelector(this.buttonID2);
-              this.itemRenderer.postRenderer(item, button1, button2);
-          }
-      }
-  }
-  
+        this.container.innerHTML = "";
+        const filteredList = this.items.filter(
+            (item) => this.filterValue === "all" || item[this.filterProperty] == this.filterValue
+        );
+        for (const item of filteredList) {
+            const html = this.itemRenderer.render(item);
+            this.container.insertAdjacentHTML("beforeend", html);
+
+            if (this.itemRenderer.postRenderer) {
+                const element = this.container.lastElementChild;
+                const button1 = element.querySelector(this.buttonID1);
+                const button2 = element.querySelector(this.buttonID2);
+                this.itemRenderer.postRenderer(item, button1, button2);
+            }
+        }
+    }
 
     sort(sortBy, sortDir) {
         if (sortDir) {
@@ -59,40 +60,39 @@ export class ListRenderer {
 
         this.render();
     }
-    filter(filterProperty, filterValue) {
-      if(filterProperty.includes(":") && filterValue === undefined) {
-        [this.filterProperty, this.filterValue] = filterProperty.split(":");
-      } else {
-        this.filterProperty = filterProperty;
-        this.filterValue = filterValue;
-      }
 
-      this.render()
+    filter(filterProperty, filterValue) {
+        if (filterProperty.includes(":") && filterValue === undefined) {
+            [this.filterProperty, this.filterValue] = filterProperty.split(":");
+        } else {
+            this.filterProperty = filterProperty;
+            this.filterValue = filterValue;
+        }
+
+        this.render();
     }
-  
-  
-  applyFilter(item) {
-      // Check if the item matches the filter criteria
-      if (this.filterProperty === "monthFilter") {
-          const month = new Date(item.date).getMonth() + 1; // Months are 0-indexed, so we add 1
-          return String(month) === this.filterValue;
-      } else {
-          // For other filter criteria, you can add additional conditions as needed
-          // For example, you might want to compare strings or numbers
-          return String(item[this.filterProperty]) === this.filterValue;
-      }
-  }
-  
-  
+
+    applyFilter(item) {
+        // Check if the item matches the filter criteria
+        if (this.filterProperty === "monthFilter") {
+            const month = new Date(item.date).getMonth() + 1; // Months are 0-indexed, so we add 1
+            return String(month) === this.filterValue;
+        } else {
+            // For other filter criteria, you can add additional conditions as needed
+            // For example, you might want to compare strings or numbers
+            return String(item[this.filterProperty]) === this.filterValue;
+        }
+    }
+
     extractSortValue(item, sortBy) {
         // Extract the value based on the sortBy parameter
         if (sortBy === "date") {
             return new Date(item.date);
         } else if (sortBy === "shiftStart") {
             return this.convertToMinutes(item.shiftStart);
-        } else if (sortBy === "timeDK"){
-          return new Date(item.timeDK);
-        }else {
+        } else if (sortBy === "timeDK") {
+            return new Date(item.timeDK);
+        } else {
             // Default to sorting by the property key
             return item[sortBy];
         }
